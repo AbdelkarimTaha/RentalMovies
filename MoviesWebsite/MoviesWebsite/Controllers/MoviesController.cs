bd785854cd5c872c286669f284c23bb5870c0143
AbdElkarim <abdelkareemtaha2014@gmail.com>
2020-02-24 22:03:50 +0200

    first

---
+++ MoviesWebsite/MoviesWebsite/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using MoviesWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesWebsite.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie {Name = "Shrek"},
+                new Movie {Name = "Wall-e"}
+            };
+            return View(movies);
+        }
+    }
+}
