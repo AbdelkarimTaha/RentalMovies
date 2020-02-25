6867caaa758915bb29c0f9a48804ed2c58ee4bde
AbdElkarim <abdelkareemtaha2014@gmail.com>
2020-02-25 09:26:23 +0200

    Enable migrations.

---
+++ MoviesWebsite/MoviesWebsite/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MoviesWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MoviesWebsite.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MoviesWebsite.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
