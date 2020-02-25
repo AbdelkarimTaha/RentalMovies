f21d22c65ad1cdb53626861bbe70670185628852
AbdElkarim <abdelkareemtaha2014@gmail.com>
2020-02-25 23:21:31 +0200

    Add a form to add/update a customer.

--- MoviesWebsite/MoviesWebsite/Controllers/CustomersController.cs
+++ MoviesWebsite/MoviesWebsite/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using MoviesWebsite.ViewModels;
 
 namespace MoviesWebsite.Controllers
 {
@@ -15,9 +16,38 @@ namespace MoviesWebsite.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        protected override void Dispose(bool disposing)
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
+
+        public ActionResult CustomerForm()
         {
-            _context.Dispose();
+            var MembershipType = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = MembershipType
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0) 
+                _context.Customers.Add(customer);
+
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
         public ActionResult Index()
         {
@@ -32,6 +62,22 @@ namespace MoviesWebsite.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- MoviesWebsite/MoviesWebsite/Models/Customer.cs
+++ MoviesWebsite/MoviesWebsite/Models/Customer.cs
@@ -16,10 +16,12 @@ namespace MoviesWebsite.Models
 
         public bool IsSubscribedToNewsletter  { get; set; }
 
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
 
         public MembershipType MembershipType { get; set; }
 
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
