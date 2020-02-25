091297a5cee3aef00e5b6315ccbf60d622bdec50
AbdElkarim <abdelkareemtaha2014@gmail.com>
2020-02-25 11:42:04 +0200

    Display customer's birthdate in Customer Details.

--- MoviesWebsite/MoviesWebsite/Controllers/CustomersController.cs
+++ MoviesWebsite/MoviesWebsite/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MoviesWebsite.Controllers
 {
@@ -20,12 +21,12 @@ namespace MoviesWebsite.Controllers
         }
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
