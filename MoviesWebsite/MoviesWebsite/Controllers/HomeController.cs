7858699aca8d41f5b80dc50a6a00db9f2a91a656
AbdelkarimTaha <abdelkareemtaha2014@gmail.com>
2020-03-31 18:12:13 +0200

    Disable session state.

--- MoviesWebsite/MoviesWebsite/Controllers/HomeController.cs
+++ MoviesWebsite/MoviesWebsite/Controllers/HomeController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MoviesWebsite.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //At the end all three might prevent caching your data but still have different meanings.
+        //[OutputCache(Duration = 0, Location = OutputCacheLocation.Client, VaryByParam = "*")]
+        //[OutputCache(NoStore = true, Duration = 0, Location = "None", VaryByParam = "*")]
+        //[OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+
+        [OutputCache(Duration =0,Location =OutputCacheLocation.Server ,VaryByParam ="*",NoStore =true)]
         public ActionResult Index()
         {
             return View();
